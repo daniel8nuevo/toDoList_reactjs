34e36f8cb9df9298b46b1e73df0619281bc49c84
daniel8nuevo <nuevo8vida@gmail.com>
2020-05-23 23:50:53 +0300

    added ToDoList component w one handleChange method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
 import React from 'react';
 import './App.css';
 
+export default class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue : null,
+      toDo: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange = e => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+  handleSubmit = e => {
+    e.preventDefault();
 
+  }
+  render(){
+    
+  }
+}

980aa22c8f57f74e3fcfe8567d9f90f3d0f68416
daniel8nuevo <nuevo8vida@gmail.com>
2020-05-24 00:11:21 +0300

    added ToDoList js and css, also renamed in app.js ToDoList class to ToDoApp class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-export default class ToDoList extends React.Component {
+export default class ToDoApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       inputValue : null,
-      toDo: []
+      todo: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,9 +18,26 @@ export default class ToDoList extends React.Component {
   }
   handleSubmit = e => {
     e.preventDefault();
+    
+    const randNumber = Math.floor(Math.random() * 9999);
 
+    const newToDo = {
+      inputValue: this.state.inputValue,
+      id: randNumber
+    }
+
+    this.setState( state => ({
+      todo:[...this.state.todo, newToDo]
+    }));
   }
   render(){
-    
+    return(
+      <div>
+        <h1>TO DO APP</h1>
+          <form onSubmit={this.handleSubmit}>
+
+          </form>
+      </div>
+    )
   }
 }
